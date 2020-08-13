eeae80e6da2d088d72a6f053e1f4d121f14bdb3a
Mike Ho <mike@quasidea.com>
2020-08-13 13:55:30 -0700

    Scaffolded initial CLI approach

--- cli.js
+++ cli.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
 
-console.log('Hi');
+function showHelp() {
+	console.log("options:\n" +
+		"   oas-client init URL\n" +
+		"   oas-client codegen"
+	);
+}
+
+if (process.argv.length < 3) {
+	showHelp();
+	return;
+}
+
+var module = require("./index");
+
+switch (process.argv[2]) {
+	case 'init':
+		if (process.argv.length !== 4) {
+			showHelp();
+			return;
+		}
+		module.init(process.argv[3]);
+		break;
+
+	case 'codegen':
+		module.codegen();
+		break;
+
+	default:
+		showHelp();
+		break;
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+exports.init = function (url) {
+	console.log('init was called on ' + url);
+};
+
+exports.codegen = function (options) {
+	console.log('codegen was called');
+};
